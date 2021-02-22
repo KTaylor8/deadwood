e59f2d7f588a09fc4174a2615db1b201ade1d849
Katie Taylor <katiemxtaylor@gmail.com>
2021-02-22 10:59:24 -0800

    init Card constructor

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,5 +1,17 @@
 import java.util.*;
 
 public class Card{
+    String cardName;
+    String budget; // that's how it is in the xml idk if we want to make it an int
+    String sceneNumber;
+    String sceneDescription;
+    Stack<Role> roles;
+
+    Card(String cardName, String budget, String sceneNumber, String sceneDescription, String cardRoles) {
+        this.cardName = cardName;
+        this.budget = budget;
+        this.sceneNumber = sceneNumber;
+        this.sceneDescription = sceneDescription;
+    }
 
 }
