a41c3a9300d8b65d1057e840ec0b90d85b14a5bf
Katie Taylor <katiemxtaylor@gmail.com>
2021-02-22 10:59:41 -0800

    init Role constructor

--- src/main/java/Role.java
+++ src/main/java/Role.java
@@ -1,5 +1,22 @@
 import java.util.*;
 
 public class Role{
+    String name;
+    String level;
+    // AreaData area; // uncomment for GUI
+    String line;
 
+
+    // Role(String name, String level, AreaData area, String line) { // uncomment for GUI
+    //     this.name = name;
+    //     this.level = level;
+    //     this.area = area;
+    //     this.line = line;
+    // }
+
+    Role(String name, String level, String line) {
+        this.name = name;
+        this.level = level;
+        this.line = line;
+    }
 }
