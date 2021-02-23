fb1eda33fec664a441656caca053fb779f1a3411
Katie Taylor <katiemxtaylor@gmail.com>
2021-02-22 20:46:04 -0800

    adding the stack of roles as a class variable

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,17 +1,18 @@
 import java.util.*;
 
 public class Card{
-    String cardName;
+    String name;
     String budget; // that's how it is in the xml idk if we want to make it an int
     String sceneNumber;
     String sceneDescription;
     Stack<Role> roles;
 
-    Card(String cardName, String budget, String sceneNumber, String sceneDescription, Stack<Role> cardRoles) {
-        this.cardName = cardName;
+    Card(String name, String budget, String sceneNumber, String sceneDescription, Stack<Role> cardRoles) {
+        this.name = name;
         this.budget = budget;
         this.sceneNumber = sceneNumber;
         this.sceneDescription = sceneDescription;
+        this.roles = cardRoles;
     }
 
 }
