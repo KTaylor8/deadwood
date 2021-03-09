4f416e7f4e42dd09b43cf05fba9b121d6bbf823b
Katie Taylor <katiemxtaylor@gmail.com>
2021-03-09 03:09:45 -0800

    create AreaData class

---
+++ src/main/java/AreaData.java
@@ -0,0 +1,35 @@
+// class for Area, so then it's easier to store and access the dimensions
+// import java.util.*;
+
+public class AreaData{
+    private int x;
+    private int y;
+    private int h;
+    private int w;
+
+    AreaData() {}
+
+    AreaData(String x, String y, String h, String w) {
+        this.x = Integer.parseInt(x);
+        this.y = Integer.parseInt(y);
+        this.h = Integer.parseInt(h);
+        this.w = Integer.parseInt(w);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+}
