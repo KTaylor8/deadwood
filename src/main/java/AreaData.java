b79eada6622e67d9bbd47d47109264ebd9449336
Katie Taylor <katiemxtaylor@gmail.com>
2021-03-10 00:08:54 -0800

    add setters for AreaData x and y

--- src/main/java/AreaData.java
+++ src/main/java/AreaData.java
@@ -27,8 +27,16 @@ public class AreaData{
         return x;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
     public int getY() {
         return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 
     public int getH() {
