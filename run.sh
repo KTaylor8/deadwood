bf90259503255459cf35fa23cc5652695033bb9f
Katie Taylor <katiemxtaylor@gmail.com>
2021-03-16 01:10:54 -0700

    update run.sh to work with 4 args (for computer players)

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ $# != 3 ]]; then
-    echo "Usage: ./run.sh BoardXML CardXML numberOfPlayers"
+if [[ $# != 3 && $# != 4 ]]; then
+    echo "Usage: ./run.sh BoardXML CardXML numberOfTotalPlayers numberOfComputerPlayers"
     exit 1
 fi
 
-java -jar deadwood.jar $1 $2 $3
+java -jar deadwood.jar $1 $2 $3 $4
